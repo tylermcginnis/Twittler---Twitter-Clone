14e7deb249dc6e985ef966bdcaefb4759115184f
Tyler McGinnis <tylermcginnis33@gmail.com>
2013-07-12 22:34:39 -0600

    add some styling. bug on show tweets text

--- data_generator.js
+++ data_generator.js
@@ -52,7 +52,7 @@ for(var i = 0; i < 10; i++){
 
 var scheduleNextTweet = function(){
   generateRandomTweet();
-  setTimeout(scheduleNextTweet, Math.random() * 1500);
+  setTimeout(scheduleNextTweet, Math.random() * 2500);
 };
 scheduleNextTweet();
 
